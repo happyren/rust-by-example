e66234d58217c0bb29b04f28b9d60ccd18730ab9
kaixiang.ren <kaixiang.ren00@gmail.com>
2023-05-29 13:14:04 +1000

    update ps1

--- build.ps1
+++ build.ps1
@@ -1,8 +1,12 @@
+# powershell script set a regular expression template
+# to match the name of the file and run the executable
+
+$filePath = $args[0]
 $reg = "([A-Za-z0-9-]*).rs$"
-rustc $matches[1] --out-dir ./bin &
-if ($matches[0] -match $reg) {
-    while (!(Test-Path "./bin/$($matches[1])")) {
+rustc $args[0] --out-dir ./bin &
+if ($args[0] -match $reg) {
+    while (!(Test-Path -Path "./bin/$($matches[1]).exe")) {
         Start-Sleep -Milliseconds 100
     }
-    ./bin/$($matches[1])
+    & ./bin/$($matches[1]).exe
 }
